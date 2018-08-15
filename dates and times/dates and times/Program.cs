657f99b82d8dd84be6988092061fe03d27584f1e
Erin Hogan <hogan10@charter.net>
2018-08-14 23:21:03 -0500

    with notes

---
+++ dates and times/dates and times/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace dates_and_times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+            // Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+            //Console.WriteLine(myValue.ToLongDateString());
+            //Console.WriteLine(myValue.ToLongTimeString());
+
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            //Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+            //Console.WriteLine(myValue.Month);
+
+            // DateTime myBirthday = new DateTime(1991, 01, 4);
+            //Console.WriteLine(myBirthday.ToShortDateString());
+
+            DateTime myBirthday = DateTime.Parse("09/21/1990");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(myAge.TotalDays);
+            Console.ReadLine();
+        }
+    }
+}
